89285db608805e67261ffbce9d9f60be78ecaf17
Phuc <54981390+ncp204@users.noreply.github.com>
2022-08-26 15:16:58 +0700

    tìm hiểu về two way binding

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,26 +1,54 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-  const [infor, setInfor] = useState({
-    name: 'Nguyen Van A',
-    age: 18,
-    address: 'Ho Chi Minh'
-  })
+// response from API
+const courses = [
+  { id: 1, name: 'HTML, CSS' },
+  { id: 2, name: 'Javascript' },
+  { id: 3, name: 'ReactJS' }
+]
 
-  const handleUpdate = () => {
-    setInfor(prev => {
-      return {
-        ...prev,
-        bio: 'aaaaaaaa'
+function App() {
+  //   const [name, setName] = useState('');
+  //   const [email, setEmail] = useState('');
+  // const [checked, setChecked] = useState();
+  const [checked, setChecked] = useState([]);
+
+  const handleRegister = () => {
+    // Call API
+    // console.log({ name, email });
+    console.log(checked);
+  }
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id);
+      if (isChecked) {
+        return checked.filter(item => item !== id);
+      } else {
+        return [...prev, id];
       }
     })
   }
 
+  console.log(checked);
+
   return (
     <div className="App">
-      <h1>{JSON.stringify(infor)}</h1>
-      <button onClick={handleUpdate}>Increase</button>
+      <div style={{ padding: 32 }}>
+        {courses.map(course => (
+          <div key={course.id}>
+            {/* <input type='radio'
+              onChange={() => setChecked(course.id)}
+              checked={checked === course.id} /> */}
+            <input type='checkbox'
+              onChange={() => handleCheck(course.id)}
+              checked={checked.includes(course.id)} />
+            {course.name}
+          </div>
+        ))}
+        <button onClick={handleRegister}>Change</button>
+      </div>
     </div>
   );
 }

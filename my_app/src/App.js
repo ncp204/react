5094d7120f1616710da0efa60b79a2b3e2c41bb3
Phuc <54981390+ncp204@users.noreply.github.com>
2022-08-26 16:04:18 +0700

    thực hành todoList

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,53 +1,36 @@
 import './App.css';
 import { useState } from 'react';
 
-// response from API
-const courses = [
-  { id: 1, name: 'HTML, CSS' },
-  { id: 2, name: 'Javascript' },
-  { id: 3, name: 'ReactJS' }
-]
-
 function App() {
-  //   const [name, setName] = useState('');
-  //   const [email, setEmail] = useState('');
-  // const [checked, setChecked] = useState();
-  const [checked, setChecked] = useState([]);
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'));
+    return storageJobs ?? [];
+  });
 
-  const handleRegister = () => {
-    // Call API
-    // console.log({ name, email });
-    console.log(checked);
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job];
+
+      // save to localstorage
+      const jsonJob = JSON.stringify(newJobs);
+      localStorage.setItem('jobs', jsonJob)
+
+      return newJobs;
+    });
+    setJob('');
   }
-
-  const handleCheck = (id) => {
-    setChecked(prev => {
-      const isChecked = checked.includes(id);
-      if (isChecked) {
-        return checked.filter(item => item !== id);
-      } else {
-        return [...prev, id];
-      }
-    })
-  }
-
-  console.log(checked);
 
   return (
     <div className="App">
       <div style={{ padding: 32 }}>
-        {courses.map(course => (
-          <div key={course.id}>
-            {/* <input type='radio'
-              onChange={() => setChecked(course.id)}
-              checked={checked === course.id} /> */}
-            <input type='checkbox'
-              onChange={() => handleCheck(course.id)}
-              checked={checked.includes(course.id)} />
-            {course.name}
-          </div>
-        ))}
-        <button onClick={handleRegister}>Change</button>
+        <input value={job} onChange={e => setJob(e.target.value)} />
+        <button onClick={handleSubmit}>Add</button>
+        <ul>
+          {jobs.map((job, index) => (
+            <li key={index}>{job}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );

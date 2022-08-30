53b6d45659b65e31bb16fa4d257dd3f091e5ec3c
Phuc <54981390+ncp204@users.noreply.github.com>
2022-08-30 11:45:28 +0700

    tìm hiểu về useReducer

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,56 +1,54 @@
 import './App.css';
-import {useState, useCallback, useMemo, useRef} from 'react';
+import {useState, useCallback, useMemo, useRef, useReducer} from 'react';
 import React from "react";
 import Content from "./Content";
 
-// useMemo: sử dụng để tránh thực hiện lại một logic không cần thiết
+// useState
+// 1. Init state: 0
+// 2. Action: Up (state +1) / Down (state -1)
+
+
+// useReducer
+// 1. Init state: 0
+// 2. Action: Up (state +1) / Down (state -1)
+// 3. Reducer
+// 4. Dispatch: kich hoat 1 action
+
+//Init state
+const initState = 0;
+
+//Acitons
+const UP_ACTION = 'up';
+const DOWN_ACTION = 'down';
+
+//Reducer
+const reducer = (state, action) => {
+    switch (action) {
+        case UP_ACTION:
+            return state + 1;
+        case DOWN_ACTION:
+            return state - 1;
+        default:
+            throw new Error('Invalid action')
+    }
+}
 
 function App() {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [products, setProducts] = useState([]);
-
-    const nameRef = useRef();
-
-    const handleSubmit = () => {
-        setProducts([...products, {
-            name,
-            price: +price
-        }])
-
-        setName('');
-        setPrice('');
-        nameRef.current.focus();
-    }
-
-    const total = useMemo(() => {
-        const result = products.reduce((result, product) => result+product.price, 0);
-        return result;
-    }, [products])
+    const [count, dispatch] = useReducer(reducer, initState);
 
     return (
         <div style={{padding: 20}}>
-            <input
-                ref={nameRef}
-                value={name}
-                placeholder="Enter your name"
-                onChange={e => setName(e.target.value)}
-            />
-            <br/>
-            <input
-                value={price}
-                placeholder="Enter price"
-                onChange={e => setPrice(e.target.value)}
-            />
-            <br/>
-            <button onClick={handleSubmit}>Add</button>
-            <br/>
-            Total: {total}
-            <ul>
-                {products.map((product, index) => (
-                    <li key={index}>{product.name} - {product.price}</li>
-                ))}
-            </ul>
+            <h1>{count}</h1>
+            <button
+                onClick={() => dispatch(DOWN_ACTION)}
+            >
+                Down
+            </button>
+            <button
+                onClick={() => dispatch(UP_ACTION)}
+            >
+                Up
+            </button>
         </div>
     );
 }

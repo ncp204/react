c40feb2a5556558d50f3d5568ed54d2f859740a4
Phuc <54981390+ncp204@users.noreply.github.com>
2022-08-30 10:15:06 +0700

    tìm hiểu về memo

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,47 +1,30 @@
 import './App.css';
 import {useEffect, useRef, useState} from 'react';
 import React from "react";
+import Content from "./Content";
 
-// Lưu các giá trị qua một tham chiếu bên ngoài
-// Function component
+// 1. memo() -> Higher Order Component (HOC)
+// 2. useCallback()
+
+// Hooks
+// HOC
+// Render props
 
 function App() {
-    const [count, setCount] = useState(60);
+    const [count, setCount] = useState(0);
 
-    const timerId = useRef();
-    const prevCount = useRef();
-
-    const h1Ref = useRef()
-
-    useEffect(() => {
-        prevCount.current = count;
-    }, [count])
-
-    useEffect(() => {
-        const rect = h1Ref.current.getBoundingClientRect();
-        console.log(rect)
-    })
-
-    const handleStart = () => {
-        timerId.current = setInterval(()=>{
-            setCount(prevState => prevState -1)
-        }, 1000)
+    const increase = () => {
+        setCount(count + 1)
     }
 
-    const handleStop = () => {
-        clearInterval(timerId.current);
-    }
-
-    console.log(count, prevCount.current)
 
     return (
-        <div style={{ padding: 20 }}>
-            <h1 ref={h1Ref}>{count}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
+        <div style={{padding: 20}}>
+            <Content/>
+            <h1>{count}</h1>
+            <button onClick={increase}>Click me</button>
         </div>
     );
 }
-
 
 export default App;
--- my_app/src/Content.js
+++ my_app/src/Content.js
@@ -1,39 +1,12 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react'
 
-/* useEffect
-1. Cập nhật lại state
-2. Cập nhật lại DOM
-3. Render lại UI
-4. Gọi cleanup nếu deps thay đổi
-5. Gọi useEffect callback
-*/
-
-/* useEffect
-1. Cập nhật lại state
-2. Cập nhật lại DOM(mutated)
-4. Gọi cleanup nếu deps thay đổi (sync)
-5. Gọi useEffectLayout callback (sync )
-6. Render lại UI
-*/
-
 function Content() {
-    const [count, setCount] = useState(0);
-
-    useLayoutEffect(() => {
-        if (count > 3)
-            setCount(0)
-    }, [count])
-
-    const handleRun = () => {
-        setCount(count + 1)
-    }
-
+    console.log('re-render')
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={handleRun}>Run</button>
+            <h1>Hello</h1>
         </div>
     )
 }
 
-export default Content;
+export default React.memo(Content);
